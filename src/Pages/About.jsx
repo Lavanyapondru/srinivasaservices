50fc5a2e4a07cc293b2cf24a3a3ac00a97c82a21
lavanya <pondrulavanya@gmail.com>
2026-07-16 15:58:28 +0530

    aboutpage

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,7 @@
 import Aboutus from "../Components/Aboutus";
-import Footer from "../components/Footer";
+
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer"
 
 function About() {
     return ( 
